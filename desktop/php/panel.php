d077f36222877666a6e93b89e26f847bc1199b70
Loïc <loic@jeedom.com>
2018-08-03 16:18:12 +0200

    typo

--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -8,26 +8,26 @@ sendVarToJs('jeedomBackgroundImg', 'plugins/gsl/core/img/panel.jpg');
     <div class="col-lg-12 col-md-12 col-sm-12" id="div_displayObject">
         <br/>
 		<?php
-		echo '<div class="div_displayEquipement" style="width: 100%;">';
-		$count = 0;
-		$eqLogic = eqLogic::byLogicalId('global', 'gsl');
-		if($eqLogic->getConfiguration('isVisiblePanel',0)) {
-			echo $eqLogic->toHtml('dview');
-			$count++;
-		}
-		$gsls = gsl::byType('gsl', true);
-		foreach ($gsls as $gsl) {
-			if ($gsl->getLogicalId() == 'global') {
-				continue;
-			}
-			if($gsl->getConfiguration('isVisiblePanel',0)) {
-				echo $gsl->toHtml('dview');
-				$count++;
-			}
-		}
-		echo '</div>';
-		sendVarToJs('nbGslWidget', $count);
-		?>
+echo '<div class="div_displayEquipement" style="width: 100%;">';
+$count = 0;
+$eqLogic = eqLogic::byLogicalId('global', 'gsl');
+if ($eqLogic->getConfiguration('isVisiblePanel', 0)) {
+	echo $eqLogic->toHtml('dview');
+	$count++;
+}
+$gsls = gsl::byType('gsl', true);
+foreach ($gsls as $gsl) {
+	if ($gsl->getLogicalId() == 'global') {
+		continue;
+	}
+	if ($gsl->getConfiguration('isVisiblePanel', 0)) {
+		echo $gsl->toHtml('dview');
+		$count++;
+	}
+}
+echo '</div>';
+sendVarToJs('nbGslWidget', $count);
+?>
     </div>
     </div>
 <?php include_file('desktop', 'panel', 'js', 'gsl');?>
