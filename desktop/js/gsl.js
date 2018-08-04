4e86bcbe5fb729b3052034610c97c45c88abff16
Yoan <kaktusatomik@hotmail.com>
2018-08-04 10:07:29 +0200

    remove global & panel checkboxes from static equipments (temporary)

--- desktop/js/gsl.js
+++ desktop/js/gsl.js
@@ -64,7 +64,10 @@ function printEqLogic(_eqLogic) {
  if(isset(_eqLogic.configuration.type) && _eqLogic.configuration.type == 'fix'){
   $('.eqLogicAttr[data-l1key=configuration][data-l2key=cmdgeoloc]').closest('.form-group').hide();
   $('.eqLogicAttr[data-l1key=configuration][data-l2key=coordinated]').closest('.form-group').show();
+  $('.eqLogicAttr[data-l1key=configuration][data-l2key=isVisibleGlobal]').closest('.form-group').hide();
+  $('.eqLogicAttr[data-l1key=configuration][data-l2key=isVisiblePanel]').closest('.form-group').hide();
 }else{
-   $('.eqLogicAttr[data-l1key=configuration][data-l2key=coordinated]').closest('.form-group').hide();      
+   $('.eqLogicAttr[data-l1key=configuration][data-l2key=coordinated]').closest('.form-group').hide();
+   $('.eqLogicAttr[data-l1key=configuration][data-l2key=isVisiblePanel]').closest('.form-group').show();
 }
 }
