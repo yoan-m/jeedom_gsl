559e5171d648ec2791983ba4bb42895a0f5a14ed
MURCIANO Yoan <y.murciano@systel-sa.com>
2020-04-30 12:16:24 +0200

    suppression des couches stadia non dispo en remote

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -18,14 +18,14 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 include_file('core', 'authentification', 'php');
 if (!isConnect()) {
-	include_file('desktop', '404', 'php');
-	die();
+    include_file('desktop', '404', 'php');
+    die();
 }
 $themes = gsl::getMapLayers();
 ?>
 <form class="form-horizontal">
     <fieldset>
-		<legend><i class="fa fa-list-alt"></i> {{Général}}</legend>
+        <legend><i class="fa fa-list-alt"></i> {{Général}}</legend>
         <div class="form-group">
             <label class="col-lg-4 control-label">{{Fréquence de rafraichissement}}</label>
             <div class="col-lg-4">
@@ -33,46 +33,46 @@ $themes = gsl::getMapLayers();
             </div>
         </div>
         <div class="form-group">
-           <label class="col-lg-4 control-label">{{Forcer déconnexion}}</label>
-           <div class="col-lg-2">
-              <a class="btn btn-danger" id="bt_logoutGsl"><i class='fas fa-sign-out-alt icon-white'></i> {{Déconnexion}}</a>
-          </div>
-      </div>
-  
+            <label class="col-lg-4 control-label">{{Forcer déconnexion}}</label>
+            <div class="col-lg-2">
+                <a class="btn btn-danger" id="bt_logoutGsl"><i class='fas fa-sign-out-alt icon-white'></i> {{Déconnexion}}</a>
+            </div>
+        </div>
+
         <div class="form-group">
             <label class="col-lg-4 control-label">{{Cookie}}</label>
-            <div class="col-lg-2">
-  				<input type="file" name="file_cookieGsl" id="file_cookieGsl">
+            <div class="col-lg-3">
+                <input type="file" style="width:100%" name="file_cookieGsl" id="file_cookieGsl">
             </div>
-  			<div class="col-lg-2">
+            <div class="col-lg-1">
                 <a class="btn btn-success" id="bt_saveCookieGsl"><i class='far fa-check-circle icon-white'></i> {{Envoyer}}</a>
             </div>
         </div>
-  </fieldset>
-  <fieldset>
-		<legend><i class="fa fa-map-marked-alt"></i> {{Carte}}</legend>
+    </fieldset>
+    <fieldset>
+        <legend><i class="fa fa-map-marked-alt"></i> {{Carte}}</legend>
         <div class="form-group">
             <label class="col-lg-4 control-label">{{Fond cartographique thème light}}</label>
             <div class="col-lg-4">
                 <select class="configKey form-control" data-l1key="light-theme">
-				<?php
-				foreach ($themes as $key => $value) {
-					echo '<option value="' . $key . '">' . $key . '</option>';
-				}
-				?>
-				</select>
+                    <?php
+                    foreach ($themes as $key => $value) {
+                        echo '<option value="' . $key . '">' . $key . '</option>';
+                    }
+                    ?>
+                </select>
             </div>
         </div>
-		<div class="form-group">
+        <div class="form-group">
             <label class="col-lg-4 control-label">{{Fond cartographique thème dark}}</label>
             <div class="col-lg-4">
                 <select class="configKey form-control" data-l1key="dark-theme">
-				<?php
-				foreach ($themes as $key => $value) {
-					echo '<option value="' . $key . '">' . $key . '</option>';
-				}
-				?>
-				</select>
+                    <?php
+                    foreach ($themes as $key => $value) {
+                        echo '<option value="' . $key . '">' . $key . '</option>';
+                    }
+                    ?>
+                </select>
             </div>
         </div>
         <div class="form-group">
@@ -87,38 +87,38 @@ $themes = gsl::getMapLayers();
                 <input type="checkbox" class="configKey form-control" data-l1key="control-attributions" />
             </div>
         </div>
-  </fieldset>
+    </fieldset>
 </form>
 <script>
     $('#bt_saveCookieGsl').on('click', function () {
-      var file = document.getElementById('file_cookieGsl').files[0];
+        var file = document.getElementById('file_cookieGsl').files[0];
         if (file) {
             // create reader
             var reader = new FileReader();
             reader.readAsText(file);
             reader.onload = function(e) {
-               $.ajax({
-                type: "POST",
-                url: "plugins/gsl/core/ajax/gsl.ajax.php",
-                data: {
-                    action: "cookie",
-                  cookie: e.target.result
-                },
-                dataType: 'json',
-                error: function (request, status, error) {
-                    handleAjaxError(request, status, error);
-                },
-                success: function (data) {
-                    if (data.state != 'ok') {
-                        $('#div_alert').showAlert({message: data.result, level: 'danger'});
-                        return;
+                $.ajax({
+                    type: "POST",
+                    url: "plugins/gsl/core/ajax/gsl.ajax.php",
+                    data: {
+                        action: "cookie",
+                        cookie: e.target.result
+                    },
+                    dataType: 'json',
+                    error: function (request, status, error) {
+                        handleAjaxError(request, status, error);
+                    },
+                    success: function (data) {
+                        if (data.state != 'ok') {
+                            $('#div_alert').showAlert({message: data.result, level: 'danger'});
+                            return;
+                        }
+                        $('#div_alert').showAlert({message: '{{Enregistement réussi}}', level: 'success'});
                     }
-                    $('#div_alert').showAlert({message: '{{Enregistement réussi}}', level: 'success'});
-                }
-            });
+                });
             };
         }
-       
+
     });
     $('#bt_logoutGsl').on('click', function () {
         $.ajax({
